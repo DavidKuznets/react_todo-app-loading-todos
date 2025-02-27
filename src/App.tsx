5398d4b1e4971e138d0a5edbc55c66eb1972d22e
DavidKuznets <davidkuznets7@gmail.com>
2025-02-27 11:43:15 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -159,12 +159,13 @@ export const App: React.FC = () => {
                 ×
               </button>
 
-              {loadingTodos.includes(todo.id) && (
-                <div data-cy="TodoLoader" className="modal overlay is-active">
-                  <div className="modal-background has-background-white-ter" />
-                  <div className="loader" />
-                </div>
-              )}
+              <div
+                data-cy="TodoLoader"
+                className={`modal overlay ${loadingTodos.includes(todo.id) ? 'is-active' : ''}`}
+              >
+                <div className="modal-background has-background-white-ter" />
+                <div className="loader" />
+              </div>
             </div>
           ))}
         </section>
